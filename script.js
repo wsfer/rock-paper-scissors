d1ab0a48f5396140bed9706b0d192cb5684fdc4b
Wesley <wesley_snaine@hotmail.com>
2021-12-13 16:14:49 -0300

    create a function to computer play

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("test");
+function computerPlay () {
+    const randomNum = Math.floor(Math.random()*3) + 1;
+    let compSelect;
+    switch (randomNum) {
+        case 1:
+            compSelect = "Rock";
+            break;
+        case 2:
+            compSelect = "Paper";
+            break;
+        case 3:
+            compSelect = "Scissor";
+            break;
+    }
+    return compSelect;
+}
