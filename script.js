60e8acb11d40767aa25e64ea499c555dec87b56f
Wesley <wesley_snaine@hotmail.com>
2021-12-13 22:43:04 -0300

    delete player play function, create playround function

--- script.js
+++ script.js
@@ -15,13 +15,43 @@ function computerPlay () {
     return compSelect;
 }
 
-function playerSelection () {
-    let playSelect = prompt("Rock, Paper or Scissor? ").toLowerCase();
-    playSelect = playSelect.replace(playSelect.slice(0,1), playSelect.slice(0,1).toUpperCase());
-    if (playSelect === "Rock" || playSelect === "Paper" || playSelect === "Scissor") {
-        return playSelect;
+const ask = prompt("Rock, Paper or Scissor? ").toLowerCase();
+const playerSelection = ask.replace(ask.slice(0,1), ask.slice(0,1).toUpperCase());
+const computerSelection = computerPlay();
+console.log(`Computer: ${computerSelection}, Player: ${playerSelection}`);
+function playRound (playerSelection, computerSelection) {
+
+    let result = null;
+
+    if (playerSelection === computerSelection) {
+        result = `Nobody won! both selected ${playerSelection}`;
+        return result;
     }
+
+    switch (playerSelection) {
+        case "Rock":
+            if (computerSelection === "Scissor") {
+                result = `You win! ${playerSelection} beats ${computerSelection}`;
+            } else if (computerSelection === "Paper") {
+                result = `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            break;
+        case "Paper":
+            if (computerSelection === "Rock") {
+                result = `You win! ${playerSelection} beats ${computerSelection}`;
+            } else if (computerSelection === "Scissor") {
+                result = `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            break;
+        case "Scissor":
+            if (computerSelection === "Paper") {
+                result = `You win! ${playerSelection} beats ${computerSelection}`;
+            } else if (computerSelection === "Rock") {
+                result = `You lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            break;
+    }
+    return result;
 }
 
-
-
+console.log(playRound(playerSelection, computerSelection));
